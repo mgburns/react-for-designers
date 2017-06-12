17add24cf34350e8824090b7296e275f3ae478fe
Mike Burns <mike.burns@upstatement.com>
2017-06-12 08:56:37 -0400

    Add React implementation

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-$("textarea").on("input", function() {
-  if ($(".js-add-photo-button").hasClass("is-on")) {
-    $("span").text(140 - 23 - $(this).val().length);
-  } else {
-    $("span").text(140 - $(this).val().length);
-  }
 
-  if ($(this).val().length > 0 || $(".js-add-photo-button").hasClass("is-on")) {
-    $(".js-tweet-button").prop("disabled", false);
-  } else {
-    $(".js-tweet-button").prop("disabled", true);
-  }
-});
+var TweetBox = React.createClass({
+  getInitialState: function() {
+    return {
+      text: "",
+      photoAdded: false
+    };
+  },
+  handleChange: function(event) {
+    this.setState({ text: event.target.value });
+  },
+  togglePhoto: function(event) {
+    this.setState({ photoAdded: !this.state.photoAdded });
+  },
+  overflowAlert: function() {
+    if (this.remainingCharacters() < 0) {
+      if (this.state.photoAdded) {
+        var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        var overflowText = this.state.text.substring(140 - 23);
+      } else {
+        var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        var overflowText = this.state.text.substring(140);
+      }
 
-$(".js-add-photo-button").on("click", function() {
-  if ($(this).hasClass("is-on")) {
-    $(this)
-      .removeClass("is-on")
-      .text("Add Photo");
-    $("span").text(140 - $("textarea").val().length);
-    if ($("textarea").val().length === 0) {
-      $(".js-tweet-button").prop("disabled", true);
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too Long:</strong>
+          &nbsp;...{beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+      return "";
     }
-  } else {
-    $(this)
-      .addClass("is-on")
-      .text("✓ Photo Added");
-    $("span").text(140 - 23 - $("textarea").val().length);
-    $(".js-tweet-button").prop("disabled", false);
+  },
+  remainingCharacters: function() {
+    if (this.state.photoAdded) {
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  },
+  render: function() {
+    return (
+      <div className="well clearfix">
+        { this.overflowAlert() }
+        <textarea className="form-control"
+                  onChange={this.handleChange}></textarea>
+        <br/>
+        <span>{ this.remainingCharacters() }</span>
+        <button className="btn btn-primary pull-right"
+          disabled={this.state.text.length === 0 && !this.state.photoAdded}>Tweet</button>
+        <button className="btn btn-default pull-right"
+          onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }
+        </button>
+      </div>
+    );
   }
 });
+
+ReactDOM.render(
+  <TweetBox />,
+  document.getElementById("container")
+);
